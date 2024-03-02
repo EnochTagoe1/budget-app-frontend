7fcbb106ed377f5cf3d4a23ae818c99775771b8e
enochtagoe <enochtagoe@pursuit.org>
2024-03-02 15:49:38 -0500

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import Items from "./Items";
+// Import ItemDetails from ".ItemsDetails";
+import ItemForm from "./ItemForm";
+
 
 const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
+ const [items, setItems] = useState([]);
+//this will toggle the item details component and send id
+const {toggleDetails, setToggleDetails} = useState({ show: false, id: null });
+const [toggleForm, setToggleForm] = useState(false);
+const [edit, setEdit] = useState({ show: false, id: null });
+
+useEffect(() => {
+  fetch("http:localhost3777/api/items")
+  .then((res) => res.json())
+  .then((data) => setItems(data.items));
+}, []);
+
+
+  return <div>
+    <h1>Budgtr App</h1>
+    {!toggleForm && (
+        <button onClick={() => setToggleForm(true)}>Create Item</button>
+      )}
+      <Items
+        setToggleDetails={setToggleDetails}
+        items={items}
+        setItems={setItems}
+        edit={edit}
+        setEdit={setEdit}
+      />
+      {toggleDetails.show && <ItemDetails toggleDetails={toggleDetails} />}
+      {(edit.show || toggleForm) && (
+        <ItemForm
+          edit={edit}
+          setEdit={setEdit}
+          setItems={setItems}
+          setToggleForm={setToggleForm}
+        />
+      )}
+
+    </div>;
 };
 
 export default App;
